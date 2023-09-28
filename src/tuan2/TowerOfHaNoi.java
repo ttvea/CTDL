bfe4d1e350554f759c87e585c815b4cbd56179a9
unknown <22130261@st.hcmuaf.edu.vn>
2023-09-28 09:28:13 +0700

    test

--- src/tuan2/TowerOfHaNoi.java
+++ src/tuan2/TowerOfHaNoi.java
@@ -1,5 +1,18 @@
 package tuan2;
 
 public class TowerOfHaNoi {
-
+	public static String moveDisk(int disk,String soure, String dest,String spare ) {
+		if(disk==0) {
+			
+			return "move disk from "+soure+ " to "+dest;
+		}else {
+			return moveDisk(disk-1, soure,spare , dest)+"\n"+
+					"move disk from "+soure+ " to "+dest+"\n"+
+					moveDisk(disk-1,spare, dest, soure);
+			
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(moveDisk(6,"thap 1", "thap 2","thap 3"));
+	}
 }
